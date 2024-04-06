072b943cc516f93ccb278b2761135201498ef7c8
sgadhar5 <149962998+sgadhar5@users.noreply.github.com>
2024-04-06 12:26:28 -0500

    Update run2.py

--- run2.py
+++ run2.py
@@ -6,7 +6,7 @@ from sklearn.metrics import accuracy_score
 import streamlit as st
 
 # Load the data
-df = pd.read_csv('/Users/succhaygadhar/Downloads/data.csv')
+df = pd.read_csv('data.csv')
 
 # Preprocess and split the data
 X_train, X_test, y_train, y_test = train_test_split(df['Sentence'], df['Sentiment'], test_size=0.2, random_state=42)
